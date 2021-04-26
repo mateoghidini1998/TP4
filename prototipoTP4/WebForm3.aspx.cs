e195c75d2ca0d0246fbdce68dcc1a56302adee61
JoaquinDilernia <81423528+JoaquinDilernia@users.noreply.github.com>
2021-04-26 12:01:44 -0300

    ej 3 avance
    avance ejercicio

---
+++ prototipoTP4/WebForm3.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace prototipoTP4
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+              if(IsPostBack == false)
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True");
+                //   cn.ConnectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True";
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from productos", cn);
+                //  cmd.CommandText = "Select * from productos";
+                // cmd.Connection = cn;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                dpwtema.DataSource = dr;
+                dpwtema.DataBind();
+
+                dpwtema.DataTextField = "NombreProducto";
+                dpwtema.DataValueField = "IdProdcuto";
+                dpwtema.DataBind();
+                cn.Close();
+
+            }
+        }
+
+    }
+}
